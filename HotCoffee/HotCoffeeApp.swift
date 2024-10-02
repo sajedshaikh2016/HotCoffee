612d32838d6b3c8fa5908afb3de39b0738fbe4ca
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-10-02 18:37:34 +0530

    Initial Commit

---
+++ HotCoffee/HotCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotCoffeeApp.swift
+//  HotCoffee
+//
+//  Created by Sajed Shaikh on 02/10/24.
+//
+
+import SwiftUI
+
+@main
+struct HotCoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
